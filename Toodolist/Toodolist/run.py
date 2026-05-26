98df5b8f2275f6ccf0986b9e86b4eefcfac82131
WederBorges <wederborgesemp@gmail.com>
2026-05-26 16:18:58 -0400

    Add files via upload

---
+++ Toodolist/Toodolist/run.py
@@ -0,0 +1,53 @@
+from app import create_app
+from database.conf import banco_prod
+from apscheduler.schedulers.background import BackgroundScheduler
+from flask import current_app
+from dotenv import load_dotenv
+import os
+from twilio.rest import Client
+from sqlalchemy import select, func, and_
+from models.models import User,Tarefas
+
+load_dotenv()
+account_sid = os.getenv('TWILIO_ACCOUNT_SID') 
+auth_token = os.getenv('TWILIO_AUTH_TOKEN') 
+
+
+app = create_app(banco_prod.DATABASE_SQLALCHEMY_URI)
+
+def enviar_mensagem():
+    with app.app_context():
+        client = Client(account_sid, auth_token)
+        with current_app.Session() as session:
+            stmt = (
+                select(func.count(Tarefas.status), User.user, User.telefone)
+                .filter(and_(User.telefone is not None, Tarefas.status=='pendente'))
+                .join(Tarefas.responsavel)
+                .group_by(User.id)
+                
+            )
+            results = session.execute(stmt).all()
+            
+            for n in results:
+                qtd = n[0]
+                nome = n[1]
+                tel = n[2]
+
+                body = f"Olá, {nome}. Você tem {qtd} tarefas pendentes"
+                message = client.messages.create(
+                    to=f'whatsapp:{tel}',
+                    from_='whatsapp:+13613265458',
+                    body=body
+                )
+                print(message)
+ 
+if __name__ == '__main__':
+
+    scheduler = BackgroundScheduler()
+
+    job = scheduler.add_job(enviar_mensagem, 'cron', hour=8, minute=30)
+    scheduler.start() 
+
+
+    app.run(debug=True)
+
