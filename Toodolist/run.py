941138a8aee497c971913f4bbf4fb489275bd6eb
Weder Videira Borges <wederborgesemp@gmail.com>
2026-06-16 19:28:33 -0400

    Realizando ultimas alterações antes de subir pro render

--- Toodolist/run.py
+++ Toodolist/run.py
@@ -1,5 +1,12 @@
 from app import create_app
 from database.conf import banco_prod
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+
 
 app = create_app(banco_prod.DATABASE_SQLALCHEMY_URI)
 
@@ -8,6 +15,6 @@ if __name__ == '__main__':
 
     app.run(
         host="0.0.0.0",
-        port=5000,
+        port=int(os.getenv("PORT", 5000)),
     )
 
