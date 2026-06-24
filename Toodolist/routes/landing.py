547184e84d88378ef6675f476d249cda114ddb9b
WederBorges <wederborgesemp@gmail.com>
2026-06-24 15:16:44 +0000

    Mudando lading page

--- Toodolist/routes/landing.py
+++ Toodolist/routes/landing.py
@@ -4,6 +4,4 @@ landing_bp = Blueprint('landing', __name__, url_prefix='/')
 
 @landing_bp.route('/', methods=['GET'])
 def landing():
-    if current_user.is_authenticated:
-        return render_template('login.html')
     return render_template('landing.html')
