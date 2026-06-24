257d61cb8a03b1dda7e78e5d18766509416e9ea4
WederBorges <wederborgesemp@gmail.com>
2026-06-24 14:38:37 +0000

    Landing Page

--- Toodolist/routes/landing.py
+++ Toodolist/routes/landing.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template
 from flask_login import current_user
 landing_bp = Blueprint('landing', __name__, url_prefix='/')
 
+@landing_bp.route('/', methods=['GET'])
 def landing_page():
     if current_user.is_authenticated:
         return render_template('login.html')
